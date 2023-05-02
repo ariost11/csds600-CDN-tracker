bad9d4b2a970fe31049a900f1c945a272aba4605
Ari Ostrow axo193@case.edu <axo193@sears.icir.org>
2023-05-02 18:28:04 -0400

    Finished traceroute analysis

--- get_traceroute_table.py
+++ get_traceroute_table.py
@@ -1,4 +1,26 @@
-
+names = ["France Telecom - Orange", "SIFY-AS-IN Sify Limited", "Akamai", "LEVEL3", "Amazon Cloudfront", "Cloudflare", "Fastly"]
 
 def rewrite_traceroute():
-    exclude = ["192.150.187.1", "169.229.0.140", "128.32.255.6", "128.32.0.38", "137.164.3.26", "137.164.11.89", "*"]
+    file = open("figures/traceroutes.txt", "w")
+    file.close()
+    for name in names:
+        exclude = ["192.150", "169.229", "128.32", "137.164", "*", "to"]
+        file = open("active_data/traceroutes/" + name + "_top50k.txt", "r")
+        lines = file.readlines()
+        map = {}
+        for line in lines:
+            ip = line.split("\n")[0].split(": ")[0]
+            quantity = line.split("\n")[0].split(": ")[1]
+            if not any(excluded_str in ip for excluded_str in exclude):
+                if not ip in map:
+                    map[ip] = 0
+                map[ip] += int(quantity)
+        top_30 = list(dict(sorted(map.items(), key=lambda item: item[1], reverse=True)).items())[0:30]
+        file.close()
+        file = open("figures/traceroutes.txt", "a")
+        file.write(name + "\n")
+        for entry in top_30:
+            file.write(entry[0] + ": " + str(entry[1]) + "\n")
+        file.close()
+
+rewrite_traceroute()
